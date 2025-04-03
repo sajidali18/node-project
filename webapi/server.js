7e1799fa031b429a5b8a976273b5ef62716ebf80
sajidali <mohammedsajid@gmail.com>
2025-04-03 10:53:05 +0530

    changes

--- webapi/server.js
+++ webapi/server.js
@@ -12,27 +12,29 @@ Database();
 
 const app = express();
 const server = http.createServer(app);
-const allowedOrigins = [
-    "http://localhost:5173",  // Vite frontend (localhost)
-    "http://localhost:3000",  // CRA frontend (localhost)
-    "https://phenomenal-moxie-0f5f18.netlify.app"  // Deployed frontend
-];
+const allowedOrigins = ["http://localhost:5173", "https://phenomenal-moxie-0f5f18.netlify.app"];
+const corsOptions = {
+    origin: allowedOrigins,
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"]
+};
+
+app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 // Explicitly set CORS headers in middleware
+app.use(express.json());
 app.use((req, res, next) => {
     const origin = req.headers.origin;
     if (allowedOrigins.includes(origin)) {
         res.setHeader("Access-Control-Allow-Origin", origin);
+        res.setHeader("Access-Control-Allow-Credentials", "true");
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
         res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-        res.setHeader("Access-Control-Allow-Credentials", "true");
-    }
-    if (req.method === "OPTIONS") {
-        return res.sendStatus(200);
     }
     next();
 });
-app.use(express.json());
 const skt = new Server(server, {
     cors: {
         origin: allowedOrigins,
