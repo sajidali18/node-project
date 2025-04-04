9697133e3497a4a5dfab94d3780485ca447b5660
sajidali <mohammedsajid@gmail.com>
2025-04-04 10:11:21 +0530

    changes

--- webapi/Controllers/Socket.js
+++ webapi/Controllers/Socket.js
@@ -1,18 +1,33 @@
-const userdata = {}
+const userdata = {};
+
 const skthandler = (skt) => {
     skt.on('connection', (socket) => {
-        console.log('user connected', socket.id);
+        console.log('User connected:', socket.id);
 
+        // Handle user joining a room
         socket.on('joinRoom', ({ room, user }) => {
             socket.join(room);
-            userdata[socket.id] = { email: user.email, name: user.First_name, socket_id: socket.id }
-            skt.to("live users").emit("updateUsers", Object.values(userdata));
-            console.log(userdata);
-            // console.log(`User ${socket.id} joined room : ${room}`);
+            userdata[socket.id] = { email: user.Email, name: user.First_Name, socket_id: socket.id };
+            skt.to(room).emit('updateUsers', Object.values(userdata));
+            console.log('Current users:', userdata);
         });
+
+        // Handle request for users in a room
+        socket.on('getUsersInRoom', (room) => {
+            const usersInRoom = Object.values(userdata).filter(user => user.room === room);
+            socket.emit('updateUsers', usersInRoom);
+        });
+
+        // Handle user disconnectn
         socket.on('disconnect', () => {
-            console.log(`user disconnected ${socket.id}`);
-        })
-    })
-}
+            const user = userdata[socket.id];
+            if (user) {
+                delete userdata[socket.id];
+                skt.to(user.room).emit('updateUsers', Object.values(userdata));
+            }
+            console.log('User disconnected:', socket.id);
+        });
+    });
+};
+
 module.exports = skthandler;
